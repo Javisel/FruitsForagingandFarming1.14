e0c9f05c6c1f24f6a85b7950bd8a85cd8354f205
Javisel <36491374+Javisel@users.noreply.github.com>
2020-01-09 10:59:09 -0500

    added black sandstone

--- src/main/resources/assets/fruits_foraging_and_farming/textures/block/ModelCoconutCrab.java
+++ src/main/resources/assets/fruits_foraging_and_farming/textures/block/ModelCoconutCrab.java
@@ -96,7 +96,7 @@ public class ModelCoconutCrab extends ModelBase {
     }
 
     @Override
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
         this.body.render(f5);
     }
 
